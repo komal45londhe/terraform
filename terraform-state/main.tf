4b4a441e4f375dd046e296827f8eb4b48c87bfc9
komal45londhe <komallondhe45@gmail.com>
2024-07-29 15:20:38 +0530

    Create main.tf

---
+++ terraform-state/main.tf
@@ -0,0 +1,18 @@
+# Configure the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+  profile = "<profile_name>"
+}
+
+# Create a S3 bucket
+resource "aws_s3_bucket" "aws_bucket" {
+  bucket		  = "${var.bucket_name}"
+}
